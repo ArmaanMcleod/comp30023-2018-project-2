daedcebac49470144fd3c8ecca3a961f4194133e
OpticGenius <opticgenius@hotmail.com>
2018-05-09 19:48:11 +1000

    Added certificate struct

--- certcheck.c
+++ certcheck.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct {
+    const char *file_path;
+    const char *url;
+} certificate_t;
+
 int main(int argc, char *argv[]) {
 
     return 0;
