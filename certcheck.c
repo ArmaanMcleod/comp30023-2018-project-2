98ab8ba3f8aee816836d167103f59ae342587287
OpticGenius <opticgenius@hotmail.com>
2018-05-12 09:10:03 +1000

    Started reading the certificates

--- certcheck.c
+++ certcheck.c
@@ -122,7 +122,7 @@ char *get_certificate_path(const char *csv_path) {
     const char separator = '/';
 
     // Split apath on last slash
-    char *const split_at = strrchr(path, separator);
+    char *split_at = strrchr(path, separator);
 
     // If slash found, split and create current directory path
     if(split_at != NULL) {
@@ -138,7 +138,37 @@ char *get_certificate_path(const char *csv_path) {
         path_to_certificates = "./";
     }
 
+    free(path);
+
     return path_to_certificates;
+}
+
+void verify_certificate(const char *cert_path) {
+    BIO *cert_bio = NULL;
+    X509 *cert = NULL;
+    X509_NAME *cert_issuer = NULL;
+    X509_CINF *cert_info = NULL;
+    STACK_OF(X509_EXTENSION) * extension_list;
+    int read_cert_bio;
+
+    // Initialise openSSL
+    OpenSSL_add_all_algorithms();
+    ERR_load_BIO_strings();
+    ERR_load_crypto_strings();
+
+    // Create BIO object to read certificate
+    cert_bio = BIO_NEW(BIO_s_file());
+
+    // Read certificate into BIO
+    read_cert_bio = BIO_read_filename(cert_bio, cert_path);
+    if (!read_cert_bio) {
+        fprintf(stderr, "Error in reading cert BIO filename\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Load certificate into bio
+
+    return;
 }
 
 int main(int argc, char *argv[]) {
