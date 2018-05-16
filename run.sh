eda7afa2d05db73e13713912d3957852102b8095
OpticGenius <opticgenius@hotmail.com>
2018-05-16 23:23:38 +1000

    Made test file look better

--- run.sh
+++ run.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-printf "LOCAL TESTS\n"
-printf "%0.s-" {1..80}
+cyan=$(tput setaf 6)
+normal=$(tput sgr0)
+magenta=$(tput setaf 5)
+yellow=$(tput setaf 3)
+
+printf "${yellow}INSTALL COLORDIFF${normal}\n"
+printf "%0.s-" {1..115}
+printf "\n"
+sudo apt install colordiff
+printf "%0.s-" {1..115}
+printf "\n"
+
+printf "${magenta}LOCAL TESTS${normal}\n"
+printf "%0.s-" {1..115}
 printf "\n"
 chmod +x test.sh
 ./test.sh
 
-printf "\nRESULT\n"
-printf "%0.s-" {1..80}
-printf "\n"
-cat result
-printf "%0.s-" {1..80}
-printf "\n"
-
-printf "\nNECTAR TESTS\n"
-printf "%0.s-" {1..80}
+printf "\n${magenta}NECTAR TESTS${normal}\n"
+printf "%0.s-" {1..115}
+printf "\n${yellow}COPYING AND RUNNING FILES${normal}\n"
+printf "${yellow}INSTALL COLORDIFF${normal}\n"
+printf "%0.s-" {1..115}
 printf "\n"
 make scp
-ssh -t uni "cd comp30023/Assignment2 && chmod +x test.sh && ./test.sh"
-
-printf "\nRESULT\n"
-printf "%0.s-" {1..80}
-printf "\n"
-cat result
-printf "%0.s-" {1..80}
-printf "\n"
+ssh -t uni "cd comp30023/Assignment2 && chmod +x test.sh && sudo apt install colordiff && ./test.sh"
--- test.sh
+++ test.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+
 # make the project
 make
 ./certcheck ./sample_certs/sample_input.csv
+make clean
+
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+normal=$(tput sgr0)
+
+printf "%0.s-" {1..115}
+printf "\nRESULT\n"
+printf "%0.s-" {1..115}
+printf "\n"
 
 # check output matches sample output
 out1="./output.csv"
 out2="./sample_certs/sample_output.csv"
-difference="$(diff -q "$out1" "$out2")"
+difference="$(colordiff -y -N "$out1" "$out2")"
 if [ -n "$difference" ]; then
-    printf "FAILED\n$difference\n" > result
+    printf "${red}FAILED${normal}\nDIFFERENCES HIGHLIGHTED BELOW\n\n$difference\n"
 else
-    printf "PASSED\n" > result
+    printf "${green}PASSED\n"
 fi
 
-make clean
+printf "%0.s-" {1..115}
+printf "\n"
